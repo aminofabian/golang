fcd6726da719bf64800d4d86e54592c5a621815c
Fabian Amino <aminofab@gmail.com>
2024-07-09 15:34:45 +0300

    declaring variables

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+
+// Declaring Variables
+var nameOne string = "Fabian";
+var nameTwo = "Amino";
+var nameThree string;
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameOne = "Harry"
+nameThree = "Faya"
+
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameFour := "Keziah"
+
+fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+
 }
